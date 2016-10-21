8dcd029ff040ba26453616c7e29414a741de55c2
darthjee <darthjee@gmail.com>
2016-10-21 19:02:54 -0200

    Start Gem

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require 'simplecov'
+SimpleCov.profiles.define 'gem' do
+  add_filter '/spec/'
+end
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
+SimpleCov.start 'gem'
+
+require 'phill'
+
+support_files = File.expand_path("spec/support/**/*.rb")
+Dir[support_files].each { |file| require file  }
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.filter_run_excluding :integration unless ENV['ALL']
+
+  config.order = 'random'
+
+  config.before do
+  end
+end
