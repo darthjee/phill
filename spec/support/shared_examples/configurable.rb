a2e46f40236cc81345cd6d986c7b1f0a7b2f50cd
darthjee <darthjee@gmail.com>
2016-10-25 11:44:01 -0200

    Validate config

--- spec/support/shared_examples/configurable.rb
+++ spec/support/shared_examples/configurable.rb
@@ -3,12 +3,19 @@ shared_examples 'a module that is configurable' do
   it { expect(described_class).to respond_to(:config=) }
 
   describe '.config' do
+    it { expect(described_class.config).to be_nil }
+
     context 'after configuration' do
+      let(:config) { {a: 1} }
       before do
-        described_class.config = {a: 1}
+        described_class.config = config
       end
 
       it { expect(described_class.config).to be_a(RecursiveOpenStruct) }
+
+      it do
+        expect(described_class.config.to_h).to eq(config)
+      end
     end
   end
 end
