35ee09db3631423080c0ed526d1a684f400df260
darthjee <darthjee@gmail.com>
2016-10-25 13:54:36 -0200

    Add Locations

--- lib/phill.rb
+++ lib/phill.rb
@@ -3,12 +3,17 @@ require 'yaml'
 
 class Phill
   require './lib/phill/aws_client'
+  require './lib/phill/location'
 
   class << self
     attr_reader :config
 
     def config=(hash)
-      @config = RecursiveOpenStruct.new(hash)
+      @config = RecursiveOpenStruct.new(hash, recurse_over_arrays: true)
+    end
+
+    def locations
+      @locations ||= config.locations.map { |l|  Location.new(l) }
     end
   end
 end
---
+++ lib/phill/location.rb
@@ -0,0 +1,2 @@
+class Phill::Location < DelegateClass(RecursiveOpenStruct)
+end
