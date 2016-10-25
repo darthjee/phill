9c525620cf46fd1f5a2b4ac0c37a26ba233f289e
darthjee <darthjee@gmail.com>
2016-10-25 17:50:11 -0200

    Add gzip compression

--- lib/phill/file.rb
+++ lib/phill/file.rb
@@ -1,8 +1,29 @@
+require 'zlib'
+
 class Phill::File
   attr_reader :path
 
   def initialize(path)
     @path = path
+  end
+
+  def compress
+    gzip_file.write(data)
+    gzip_file.close
+  end
+
+  private
+
+  def compressed_path
+    @zip_name ||= "#{path}.gz"
+  end
+
+  def gzip_file
+    @gzip_file ||= Zlib::GzipWriter.open(compressed_path)
+  end
+
+  def data
+    @data ||= File.read(path)
   end
 end
 
